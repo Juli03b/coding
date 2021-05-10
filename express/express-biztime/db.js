d5e192f71f0105c25bd8b12768320bfb221bbe36
Julio Barrios <julio.junior02@gmail.com>
2021-05-10 17:20:03 -0500

    Remove unecessary text

--- express/express-biztime/db.js
+++ express/express-biztime/db.js
@@ -2,9 +2,9 @@ const { Client } = require("pg");
 let DB_URI;
 
 if (process.env.NODE_ENV === "test"){
-    DB_URI = "postgresql://julio:jones0722@localhost:5432/biztime_test";
+    DB_URI = "postgresql:///biztime_test";
 }else{
-    DB_URI = "postgresql://julio:jones0722@localhost:5432/biztime";
+    DB_URI = "postgresql:///biztime";
 }
 
 let db = new Client({
